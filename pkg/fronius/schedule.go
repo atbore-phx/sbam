a729b3633a0176aa06409f455741b288f590ff02
atbore-phx <atbore@gmail.com>
2024-03-05 15:29:26 +0100

    feat(main): adding minimum battery reserve

--- pkg/fronius/schedule.go
+++ pkg/fronius/schedule.go
@@ -5,40 +5,39 @@ import (
 	"time"
 )
 
-func SetFroniusChargeBatteryMode(pw_forecast float64, pw_batt2charge float64, pw_batt_max float64, pw_consumption float64, max_charge int, start_hr string, end_hr string, fronius_ip string, fronius_port ...string) (int16, error) {
+func SetFroniusChargeBatteryMode(pw_forecast float64, pw_batt2charge float64, pw_batt_max float64, pw_consumption float64, max_charge float64, pw_batt_reserve float64, start_hr string, end_hr string, fronius_ip string, fronius_port ...string) (int16, error) {
 	p := "502"
 	if len(fronius_port) > 0 {
 		p = fronius_port[0]
 	}
 	var ch_pc int16 = 0
-	pw_grid := 0.0
+	pw_pv_net := pw_forecast - pw_consumption // Net solar power
+	pw_batt := pw_batt_max - pw_batt2charge   // actual battery power
+
 	time, _ := CheckTimeRange(start_hr, end_hr)
 
-	if !time { // out of the time range => do not charge
+	switch {
+	case !time: // out of the time range => do not charge
 		u.Log.Infof("Out time range start_time: %s - end_time: %s", start_hr, end_hr)
 		Setdefaults(fronius_ip, p)
-	} else if pw_batt2charge == 0 { // battery 100% => do not charge
-		u.Log.Infof("Battery to charge: %f %%", pw_batt2charge)
+	case pw_batt2charge == 0: // battery 100% => do not charge
+		u.Log.Info("Battery is full charged")
 		Setdefaults(fronius_ip, p)
-	} else { // in the time range
-		pw_pv_net := pw_forecast - pw_consumption
-		if pw_pv_net <= 0 { // net pv power is not enough
-			u.Log.Infof("Net Forecast Power is not enough: %f W", pw_pv_net)
-			pw_batt := float64(pw_batt_max) - pw_batt2charge
-			pw_grid = pw_batt + pw_pv_net
-			if pw_grid < 0 { // Battery Capacity is not enough => charge the diff
-				u.Log.Infof("Battery Capacity is not enough: %f W", pw_batt)
-				ch_pc = SetChargePower(float64(pw_batt_max), -1*pw_grid, float64(max_charge))
-				ForceCharge(fronius_ip, ch_pc, p)
-			} else { // Battery Capacity is enough => do not charge
-				u.Log.Infof("Battery Capacity is enough: %f W", pw_batt)
-				Setdefaults(fronius_ip, p)
-			}
-		} else { // net pv power is enough => do not charge
-			u.Log.Infof("Net Forecast Power is enough: %f W", pw_pv_net)
+	case pw_batt < pw_batt_reserve: // battery is less than reserve => charge
+		ch_pc = SetChargePower(pw_batt_max, pw_batt_reserve, max_charge)
+		ForceCharge(fronius_ip, ch_pc, p)
+
+	default:
+		pw_grid, charge_enabled := ChargeBattery(pw_pv_net, pw_batt)
+		if charge_enabled {
+			ch_pc = SetChargePower(pw_batt_max, -1*pw_grid, max_charge)
+			ForceCharge(fronius_ip, ch_pc, p)
+		} else {
 			Setdefaults(fronius_ip, p)
 		}
+
 	}
+
 	return ch_pc, nil
 }
 
@@ -46,6 +45,25 @@ func SetChargePower(max float64, load float64, limit float64) int16 {
 
 	return int16(min(load*100/max, limit*100/max))
 
+}
+
+func ChargeBattery(pw_pv_net float64, pw_batt float64) (float64, bool) {
+	enabled := false
+	pw_grid := pw_batt + pw_pv_net
+
+	if pw_pv_net <= 0 { // net pv power is not enough
+		u.Log.Infof("Net Forecast Power is not enough: %f W", pw_pv_net)
+		if pw_grid < 0 { // Battery Capacity is not enough => charge the diff
+			u.Log.Infof("Battery Capacity is not enough: %f W", pw_batt)
+			enabled = true
+		} else { // Battery Capacity is enough => do not charge
+			u.Log.Infof("Battery Capacity is enough: %f W", pw_batt)
+		}
+	} else { // net pv power is enough => do not charge
+		u.Log.Infof("Net Forecast Power is enough: %f W", pw_pv_net)
+	}
+
+	return pw_grid, enabled
 }
 
 func CheckTimeRange(start_hr string, end_hr string) (bool, error) {
