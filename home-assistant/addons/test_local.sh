d15ef76d03688cc272f477a27ec8ae56ec18d218
atbore-phx <atbore@gmail.com>
2024-03-07 15:55:17 +0100

    feat(ha): init ha add-on

---
+++ home-assistant/addons/test_local.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BUILDCONTAINER_DATA_PATH="/data"
+PATHTOBUILD="$BUILDCONTAINER_DATA_PATH"
+ARCH=amd64
+
+
+PROJECTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/sbam
+ROOTDIR=$PROJECTDIR/../../../
+cd $PRROOTDIROJECTDIR/../../../
+make build
+cd -
+
+
+rm -rf $PROJECTDIR/bin
+mkdir -p $PROJECTDIR/bin
+cp $ROOTDIR/bin/sbam $PROJECTDIR/bin/
+
+echo "project directory is $PROJECTDIR"
+echo "build container data path is $BUILDCONTAINER_DATA_PATH"
+echo "build container target build path is $PATHTOBUILD"
+CMD="docker run --rm -ti --name hassio-builder --privileged -v $PROJECTDIR:$BUILDCONTAINER_DATA_PATH -v /var/run/docker.sock:/var/run/docker.sock:ro homeassistant/amd64-builder --target $PATHTOBUILD --$ARCH --test --docker-hub local"
+echo "$CMD"
+$CMD
