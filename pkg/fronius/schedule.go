891276f1de7cb7ebd30337a704b98223ab52a2fd
atbore-phx <atbore@gmail.com>
2024-04-01 17:51:02 +0200

    fix(schedule): added case when battery is under reserve and net pv power is not enough

--- pkg/fronius/schedule.go
+++ pkg/fronius/schedule.go
@@ -12,14 +12,18 @@ func SetFroniusChargeBatteryMode(pw_forecast float64, pw_batt2charge float64, pw
 	var ch_pc int16 = 0
 	pw_pv_net := pw_forecast - pw_consumption // Net solar power
 	pw_batt := pw_batt_max - pw_batt2charge   // actual battery power
+	pw_net := pw_batt_reserve + pw_pv_net     // net power available (reserve + Net solar power)
 
 	switch {
 	case pw_batt2charge == 0: // battery 100% => do not charge
 		u.Log.Info("Battery is full charged")
+	case pw_net < 0 && pw_batt < pw_batt_reserve: // net power is not enough and battery level is under the reserve => charge
+		u.Log.Infof("Battery is under the reserve and Net Power (battery reserve + Net solar power) is not enough: %f W", pw_net)
+		ch_pc = SetChargePower(pw_batt_max, -1*pw_net, max_charge)
 	case pw_batt < pw_batt_reserve: // battery is less than reserve => charge
 		u.Log.Infof("battery %f W < reserve %f W", pw_batt, pw_batt_reserve)
 		ch_pc = SetChargePower(pw_batt_max, pw_batt_reserve-pw_batt, max_charge)
-	default:
+	default: // check if Actual battery is enough, charge oterwise
 		pw_grid, charge_enabled := ChargeBattery(pw_pv_net, pw_batt)
 		if charge_enabled {
 			ch_pc = SetChargePower(pw_batt_max, -1*pw_grid, max_charge)
